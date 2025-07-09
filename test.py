0684fdc233c158838b9abcd5b75a435ca9c774c3
ztdhhhh <166409961+ztdhhhh@users.noreply.github.com>
2025-07-09 23:08:50 +0800

    test.py

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
-print("test11")
+print("This is a test file.")
 print("sly is ok")
+print("new line added by you")  # 新增行
 print("zyl is ok")
-print("hfjdgh")
 print("ztd is ok")
-print("hdfjh")
-
+print("hh")
+print("good night")
+print("hello")
